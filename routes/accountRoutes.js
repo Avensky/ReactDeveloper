fe5cab0f7ce2f33159e60c227512fd3b6fa11dfb
uriel zacarias <urielzacarias@gmail.com>
2020-05-15 00:52:31 -0700

    added server validation for existing emails

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -3,7 +3,7 @@ const user = require('../models/User');
 const User = mongoose.model('users');
 
 module.exports = app => {
-    app.get('/api/users', (req,res) =>{          //get all posts info from db
+    app.get('/api/users', (req,res) =>{          //get all users info from db
         User.find({},(err,doc)=>{
             if(doc)
                 res.json(doc);
@@ -14,25 +14,43 @@ module.exports = app => {
     });
 
     app.post('/api/addUser',(req,res)=>{        //add a new post
-        const userObj = new User({
-            username : req.body.username, 
-            givenName : req.body.givenName, 
-            familyName : req.body.familyName, 
-            email : req.body.email, 
-            password : req.body.password,
-            picture: req.body.picture,
-            date: new Date()
-        })
-        userObj.save((err)=>{
-            if(err){
-            console.log(err);
-            res.send('Unable to save user data!');
-            }
-            else
-            res.send('User data saved successfully!');
-        })
-    });
+        console.log('user signup');
+        const { username, givenName, familyName, email, password, picture} = req.body
 
+        // VALIDATION
+
+        User.findOne({ email: email }, (err, user) => {
+            if (err) {
+                console.log('User.js post error: ', err)
+            } else if (user) {
+                res.json({
+                    error: `I'm sorry, this email already exists: ${email}`
+                })
+            }
+            else {
+                const newUser = new User({
+                    username : username, 
+                    givenName : givenName, 
+                    familyName : familyName, 
+                    email : email, 
+                    password : password,
+                    picture: picture,
+                    date: new Date()
+                })
+                newUser.save((err, savedUser) => {
+                    if (err) {
+                        console.log(err);
+                        res.send('Unable to save user data!');
+                        res.json(err)
+                    }
+                    else
+                        res.send('User data saved successfully!');
+                        res.json(savedUser)
+                })
+            }
+        })
+    })
+    
     app.get('/api/getuserDetails/:userid',(req,res)=>{              //get user details
         User.findOne({userId : req.body.userid},{},(err,doc)=>{
             if(doc)
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,31 +4,6 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 module.exports = app => {
-  app.post('/', (req, res) => {
-    console.log('user signup');
-
-    const { username, password } = req.body
-    // ADD VALIDATION
-    User.findOne({ username: username }, (err, user) => {
-        if (err) {
-            console.log('User.js post error: ', err)
-        } else if (user) {
-            res.json({
-                error: `Sorry, already a user with the username: ${username}`
-            })
-        }
-        else {
-            const newUser = new User({
-                username: username,
-                password: password
-            })
-            newUser.save((err, savedUser) => {
-                if (err) return res.json(err)
-                res.json(savedUser)
-            })
-        }
-    })
-})
 
   app.post(
     '/api/login',
@@ -47,7 +22,7 @@ module.exports = app => {
   })
 
   app.get(
-    '/', (req, res, next) => {
+    '/api', (req, res, next) => {
       console.log('===== user!!======')
       console.log(req.user)
     if (req.user) {
