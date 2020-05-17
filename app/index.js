5ffc8cee02bc2785ef6168fef0ecb7466cd99d33
uriel zacarias <urielzacarias@gmail.com>
2020-05-16 23:00:04 -0700

    created local login test in server

--- app/index.js
+++ app/index.js
@@ -1,17 +1,18 @@
 const path = require('path')
 const express = require('express')
 //app.use(express.json())
-const exphbs = require('express-handlebars')
+//const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 // app.use(bodyParser.json())
-const passport = require('passport')
+
 // require('./services/passport');
-const redis = require('redis')
+const flash = require('connect-flash');
+
 const session = require('express-session')
 // const cookieSession = require('cookie-session');
-let RedisStore = require('connect-redis')(session)
-let redisClient = redis.createClient()
-
+const redis = require('redis')
+//let RedisStore = require('connect-redis')(session)
+//let redisClient = redis.createClient()
 // let redisClient = redis.createClient({
 //     host: 'localhost',
 //     port: 6123,
@@ -26,42 +27,71 @@ let redisClient = redis.createClient()
 const config = require('../config')
 const app = express()
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded({extended: false})) // get information from html forms
 
 require('./authentication').init(app)
 
-app.use(session({
-//    store: new RedisStore({url: config.redisStore.url}),
-    store: new RedisStore({ client: redisClient }),
-//    secret: config.redisStore.secret,
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-}))
+// app.use(session({
+// //    store: new RedisStore({url: config.redisStore.url}),
+//     store: new RedisStore({ client: redisClient }),
+// //    secret: config.redisStore.secret,
+//     secret: 'keyboard cat',
+//     resave: false,
+//     saveUninitialized: false
+// }))
 
-// app.use(
-//     cookieSession({
-//         maxAge: 30*24*60*60*1000,
-//         keys: [keys.cookieKey]
-//     })
-// )
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000,
+        keys: [keys.cookieKey]
+    })
+)
 
+// pass passport for configuration
+app.use(session({ secret: 'secret' })); // session secret
 app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.session()) // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-app.engine('.hbs', exphbs({
-    defaultLayout: 'layout',
-    extname: '.hbs',
-    layoutsDir: path.join(__dirname),
-    partialsDir: path.join(__dirname)
-}))
+app.use(morgan('dev')); // log every request to the console
 
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname))
+// app.engine('.hbs', exphbs({
+//     defaultLayout: 'layout',
+//     extname: '.hbs',
+//     layoutsDir: path.join(__dirname),
+//     partialsDir: path.join(__dirname)
+// }))
+// 
+// app.set('view engine', '.hbs')
+// app.set('views', path.join(__dirname))
+// 
+// require('./user').init(app)
+// require('./note').init(app)
 
-require('./user').init(app)
-require('./note').init(app)
+const keys = require('../config/keys');
+// db
+const mongoose = require('mongoose');
+require('./models/User');
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true,useUnifiedTopology: true })
+    .then(connect => console.log('connected to mongodb'))
+    .catch(err => console.log('could not connect to mongodb', err))
+module.exports = {mongoose}
+
+
+// let db = mongoose.connection;
+// db.on('error', ()=>{
+//     console.error('Unable to connect MongoDB!')
+// });
+// db.once('open', ()=> {
+//     console.log('Connected to mongoDB!');
+// });
+
+// routes ======================================================================
+require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./routes/authRoutes')(app);
+require('./routes/postRoutes')(app);
+require('./routes/accountRoutes')(app);
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,28 +1,61 @@
-const app = require('./app')
+// server.js
 
-const keys = require('./config/keys');
-// db
-const mongoose = require('mongoose');
-require('./models/User');
-mongoose.Promise = global.Promise;
+// set up ======================================================================
+// const app = require('./app')
+const express    = require('express')
+const app        = express()
+const PORT       = process.env.PORT || 5000;
+const bodyParser = require('body-parser')
+const session    = require('express-session')
+const passport   = require('passport')
+const flash      = require('connect-flash')
+const mongoose   = require('mongoose')
+const keys       = require('./config/keys')
+
+// configuration ===============================================================
+require('./app/models/User');
+mongoose.Promise = global.Promise;// connect to our database
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true,useUnifiedTopology: true })
     .then(connect => console.log('connected to mongodb'))
     .catch(err => console.log('could not connect to mongodb', err))
 module.exports = {mongoose}
+// let db = mongoose.connection;
+// db.on('error', ()=>{
+//     console.error('Unable to connect MongoDB!')
+// });
+// db.once('open', ()=> {
+//     console.log('Connected to mongoDB!');
+// });
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+//app.use(logger('dev')); // log every request to the console
+//app.use(cookieParser()); // read cookies (needed for auth)
+// app.use(
+//     cookieSession({
+//         maxAge: 30*24*60*60*1000,
+//         keys: [keys.cookieKey]
+//     })
+// )
+app.use(bodyParser.urlencoded({extended: false})) // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ 
+    secret: 'ilovescotchscotchyscotchscotch',
+    resave: false,
+    saveUninitialized: false
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 
-let db = mongoose.connection;
-db.on('error', ()=>{
-    console.error('Unable to connect MongoDB!')
-});
-db.once('open', ()=> {
-    console.log('Connected to mongoDB!');
-});
-
-require('./routes/authRoutes')(app);
-require('./routes/postRoutes')(app);
-require('./routes/accountRoutes')(app);
-
+// launch ======================================================================
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
     // like our main.js file, or main.css file!
@@ -36,10 +69,9 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, (err) =>{
     if(!err)
         console.log('server started running on:' + PORT);
     else
-        console.log('unable to start server');
-});
+        console.log('unable to start server');    
+})
