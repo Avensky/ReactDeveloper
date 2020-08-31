2b42e09dc70328235be1c2cab4649540a9956f34
uriel zacarias <urielzacarias@gmail.com>
2020-08-31 12:05:38 -0700

    added links to user

--- client/src/containers/Pages/Home/Home.js
+++ client/src/containers/Pages/Home/Home.js
@@ -232,32 +232,32 @@ class Home extends Component {
                     <div className="container">
                         <div className="row">
                             <div className="col l6 s12">
-                                <h5 className="white-text">Company Bio</h5>
-                                <p className="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-                            </div>
-                            <div className="col l3 s12">
-                                <h5 className="white-text">Settings</h5>
-                                <ul>
-                                    <li><a className="white-text" href="#!">Link 1</a></li>
-                                    <li><a className="white-text" href="#!">Link 2</a></li>
-                                    <li><a className="white-text" href="#!">Link 3</a></li>
-                                    <li><a className="white-text" href="#!">Link 4</a></li>
-                                </ul>
+                                <h5 className="white-text">Uriel Zacarias Bio</h5>
+                                <p className="grey-text text-lighten-4">I’m currently looking for a junior web developer position. For the last two years I’ve been learning and practicing Full Stack Web Development and my strongest framework is React.</p>
                             </div>
                             <div className="col l3 s12">
                                 <h5 className="white-text">Connect</h5>
                                 <ul>
-                                    <li><a className="white-text" href="#!">Link 1</a></li>
-                                    <li><a className="white-text" href="#!">Link 2</a></li>
-                                    <li><a className="white-text" href="#!">Link 3</a></li>
-                                    <li><a className="white-text" href="#!">Link 4</a></li>
+                                    <li><a className="white-text" href="https://urielzacarias.herokuapp.com/"   >Personal Website</a></li>
+                                    <li><a className="white-text" href="https://github.com/avensky"             >GitHub</a></li>
+                                    <li><a className="white-text" href="https://linkedin.com/in/urielzacarias"  >LinkedIn</a></li>
+                                    <li>Email: urielzacarias@gmail.com</li>
+                                    <li>Phone: 619-621-7311</li>
+                                </ul>
+                            </div>
+                            <div className="col l3 s12">
+                                <h5 className="white-text">Side Projects</h5>
+                                <ul>
+                                    <li><a className="white-text" href="https://www.youtube.com/channel/UCyyo9pq7jcUaXzF7FngMqkg">Gaming and Video Editing - Youtube</a></li>
+                                    <li><a className="white-text" href="https://www.youtube.com/channel/UCylRD_yUFN_KmU5SJgUadiA">Song covers - Youtube</a></li>
+                                    <li><a className="white-text" href="http://avenskypro.blogspot.com/">Tech and Gaming Blog</a></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                     <div className="footer-copyright">
                         <div className="container">
-                            Made by <a className="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+                            
                         </div>
                     </div>
                 </footer>
